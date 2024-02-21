02548dae27f0f3d32aad4c9f2ced40785bd3c010
danielnevesfonseca <daniel.neves.fonseca.ti@gmail.com>
2024-02-21 19:33:26 -0300

    feat: add auth service and intall cookie-section lib

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,19 +14,29 @@ import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDTO } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDTO)
 export class UsersController {
-  constructor(private userService: UsersService) {}
-  @Get()
-  findAllUsers(@Query('email') email: string) {
-    return this.userService.find(email);
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDTO) {
+    return this.authService.signin(body.email, body.password);
   }
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDTO) {
-    this.userService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
   }
 
   @Get('/:id')
