3566a788b8acdf3bf7c5372ecaf047d0e3bc015c
danielnevesfonseca <daniel.neves.fonseca.ti@gmail.com>
2024-02-28 10:49:41 -0300

    feat: resolve production setting

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,45 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-export const appDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'db.dev.sqlite',
-  entities: ['**/*.entity{.js,.ts}'],
-  migrations: [__dirname + '/migrations/*{.js,.ts}'],
-} as DataSourceOptions);
+const settings = (): DataSourceOptions => {
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
+
+  if (nodeEnv === 'test') {
+    return {
+      type: 'sqlite',
+      database: 'db.test.sqlite',
+      synchronize: true,
+      entities: ['**/*.entity.{js,ts}'],
+    };
+  } else if (nodeEnv === 'development') {
+    return {
+      type: 'sqlite',
+      database: 'db.dev.sqlite',
+      synchronize: true,
+      entities: ['**/*.entity.{js,ts}'],
+    };
+  } else if (nodeEnv === 'production') {
+    const dbUrl: string | undefined = process.env.DATABASE_URL;
+
+    if (!dbUrl) throw new Error("Missing env var: 'DATABASE_URL'");
+
+    return {
+      type: 'postgres',
+      url: dbUrl,
+      logging: true,
+      entities: ['**/*.entity.{js,ts}'],
+      migrations: [__dirname + './migrations/**.{ts,js}'],
+      synchronize: false,
+    };
+  }
+};
+
+const appDataSource = new DataSource(settings());
+
+export { appDataSource };
+
+// export const appDataSource = new DataSource({
+//   type: 'sqlite',
+//   database: 'db.dev.sqlite',
+//   entities: ['**/*.entity{.js,.ts}'],
+//   migrations: [__dirname + '/migrations/*{.js,.ts}'],
+// } as DataSourceOptions);
